ce892143b5c73d24398974c3a12d18eae6baa0cf
MUHAMAD JUNAIDY MAT YUSOF <2023664176@isiswa.uitm.edu.my>
2025-12-10 12:06:07 +0800

    event

--- PACOM.WebhhookService/obj/Debug/net9.0/PACOM.WebhhookService.AssemblyInfo.cs
+++ PACOM.WebhhookService/obj/Debug/net9.0/PACOM.WebhhookService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PACOM.WebhhookService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2031a50e957cc467805b6f80c016fbd7d4f7482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de644bb77e25903c8d8468789b188ac79236257d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PACOM.WebhhookService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PACOM.WebhhookService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
